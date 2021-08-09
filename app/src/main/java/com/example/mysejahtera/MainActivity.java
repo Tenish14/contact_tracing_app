d3930b6268df558e686ddded3c77d30249859d58
Asher <asherchan2002@hotmail.com>
2021-08-09 09:28:46 +0800

    Add splash screen

--- app/src/main/java/com/example/mysejahtera/MainActivity.java
+++ app/src/main/java/com/example/mysejahtera/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.mysejahtera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler h = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_one);
+        setContentView(R.layout.activity_main);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, sign_up_one.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
 }
