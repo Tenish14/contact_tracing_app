27a97df7eb8cfaa7932a0d0884bac95db5a0af95
Tenish <tenish02@gmail.com>
2021-08-16 20:23:04 +0800

    vaccine_education_page

--- app/src/main/java/com/example/mysejahtera/DashboardFragment.java
+++ app/src/main/java/com/example/mysejahtera/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.mysejahtera;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +16,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DashboardFragment extends Fragment {
+    ImageButton vaccine_education;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +62,17 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        vaccine_education = (ImageButton)view.findViewById(R.id.vaccine_education);
+        vaccine_education.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), VaccineEducation.class));
+            }
+        });
+
+        return view;
     }
+
 }
---
+++ app/src/main/java/com/example/mysejahtera/VaccineEducation.java
@@ -0,0 +1,14 @@
+package com.example.mysejahtera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VaccineEducation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vaccine_education);
+    }
+}
